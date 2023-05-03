7cb05c4cd4d22bb85bb2b8e914142e7d1c5b2d17
Marco Ollivier <marcopollivier@gmail.com>
2023-05-03 15:48:24 -0300

    Adiciona alterações na aula 1 e cria aula 2

---
+++ class-2/primeiro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void imprimeNumero(int numero);
+
+int main() {
+    imprimeNumero(5);
+    return 0;
+}
+
+void imprimeNumero(int numero){
+    printf("%d %d %d %d", numero, 1, 3, 1990);
+}
